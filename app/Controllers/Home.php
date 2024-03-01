e03df6339c208fa32a2be28bf908652c7845080c
Abdalhaqq M Saih <abdalhaqqm@gmail.com>
2024-03-01 14:48:42 +0700

    hak cuti

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -17,13 +17,18 @@ class Home extends BaseController
         $user = get_user();
         $employees = EmployeeModel::count();
         $now = time_now();
+        $hakCuti = 17;
+        $cutiApproved = ProposalModel::whereHas('action', function ($query) {
+            $query->where('status', 'approved');
+        })->where('type', 'leave')->where('employee_id', $user->id)->count();
+        $sisaCuti = $hakCuti - $cutiApproved;
         $today = ProposalModel::where('date_start', '<=', $now)
             ->where('date_end', '>=', $now)
             ->get();
         $leaveCount = $today->where('type', 'leave')->count();
         $visitCount = $today->where('type', 'visit')->count();
         $lateCount  = $today->where('type', 'late')->count();
-        return view('home', compact('title', 'user', 'leaveCount', 'visitCount', 'lateCount'));
+        return view('home', compact('title', 'user', 'leaveCount', 'visitCount', 'lateCount', 'hakCuti', 'sisaCuti', 'cutiApproved'));
     }
 
     public function forbidden(): string
--- app/Views/home.php
+++ app/Views/home.php
@@ -3,6 +3,42 @@
 <?= $this->section('content') ?>
 <!-- Content -->
 <div class="p-6">
+    <?php if (!has_role(['SPV'])) : ?>
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
+        <div class="bg-white rounded-md border border-gray-100 p-6 shadow-md shadow-black/5">
+            <div class="flex justify-between mb-6">
+                <div>
+                    <div class="flex items-center mb-1">
+                        <div class="text-2xl font-semibold"><?= $hakCuti ?></div>
+                    </div>
+                    <div class="text-sm font-medium text-gray-400">Hak Cuti Tahunan</div>
+                </div>
+            </div>
+
+            
+        </div>
+        <div class="bg-white rounded-md border border-gray-100 p-6 shadow-md shadow-black/5">
+            <div class="flex justify-between mb-4">
+                <div>
+                    <div class="flex items-center mb-1">
+                        <div class="text-2xl font-semibold"><?= $cutiApproved ?></div>
+                    </div>
+                    <div class="text-sm font-medium text-gray-400">Approved</div>
+                </div>
+            </div>
+            
+        </div>
+        <div class="bg-white rounded-md border border-gray-100 p-6 shadow-md shadow-black/5">
+            <div class="flex justify-between mb-6">
+                <div>
+                    <div class="text-2xl font-semibold mb-1"><?= $sisaCuti ?></div>
+                    <div class="text-sm font-medium text-gray-400">Sisa</div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    <?php endif; ?>
     <div class="grid grid-cols-1 gap-6 mb-6">
         <div class="bg-white rounded-md border border-gray-100 p-6 shadow-md shadow-black/5">
             <div class="flex justify-between mb-6">
